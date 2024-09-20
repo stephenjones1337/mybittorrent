0e62a2d605b0f14b4a8647adeec5bcfde33c8a46
stephenjones1337 <stevejonesesemail@gmail.com>
2024-09-20 15:52:25 -0500

    list test

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using System.Text.Json;
 
 // Parse arguments
@@ -19,22 +20,46 @@ if (command == "decode")
     if (Char.IsDigit(encodedValue[0]))
     {
         // Example: "5:hello" -> "hello"
-        var colonIndex = encodedValue.IndexOf(':');
-        if (colonIndex != -1)
-        {
-            var strLength = int.Parse(encodedValue[..colonIndex]);
-            var strValue = encodedValue.Substring(colonIndex + 1, strLength);
-            Console.WriteLine(JsonSerializer.Serialize(strValue));
-        }
-        else
-        {
-            throw new InvalidOperationException("Invalid encoded value: " + encodedValue);
-        }
+        Console.WriteLine(DecodeString(encodedValue, out int strLength));
     }
     else if (Char.IsAsciiLetter(encodedValue[0]) && Char.IsAsciiLetter(encodedValue[^1])) // returns false for capital letters as well
     {
-        // Example: i52e
-        Console.WriteLine(JsonSerializer.Serialize(Int128.Parse(encodedValue[1..^1])));
+        //lists
+        if (encodedValue[0] == 'l' && encodedValue[^1] == 'e') //EX: l5:helloi52ee
+        {
+            // loop until reach e
+            //set current index
+            var currentIndex = 1;
+
+            while (currentIndex < encodedValue.Length-1)
+            {
+                // remaking first 'if' logic, come up with reusable way...
+                if (Char.IsDigit(encodedValue[currentIndex]))
+                {
+                    var str = DecodeString(encodedValue, out int strLength ,currentIndex);
+                    currentIndex += strLength;
+                    Console.WriteLine(str);
+                }
+                else if (encodedValue[currentIndex] == 'i') // i guess i'm hoping that only ints start with i...
+                {
+                    var intValue = DecodeInteger(encodedValue, out currentIndex, currentIndex);
+                    Console.WriteLine(intValue);
+                }
+                else
+                {
+                    //this should be end... we should break anyway to prevent recursion.
+                    //just throw error as we should never be here
+                    throw new InvalidDataException($"Data malformed, unable to continue processing loop. Current index: {currentIndex} - Current value: {encodedValue[currentIndex]} - Encoded value: " + encodedValue);
+                }
+            }
+
+            var colonIndex = encodedValue.IndexOf(':');
+        }
+        else
+        {
+            // Example: i52e
+            Console.WriteLine(JsonSerializer.Serialize(DecodeInteger(encodedValue, out int currentIndex)));
+        }
     }
     else
     {
@@ -44,4 +69,42 @@ if (command == "decode")
 else
 {
     throw new InvalidOperationException($"Invalid command: {command}");
+}
+
+
+
+static Int128 DecodeInteger(string encodedValue, out int currentIndex,  int startIndex = 1)
+{
+    int endIndex = startIndex;
+    foreach (char c in encodedValue)
+    {
+        if (Char.IsDigit(c))
+            endIndex++;
+        else if (Char.IsLetter(c))
+            break;
+    }
+    currentIndex = endIndex;
+    if (Int128.TryParse(encodedValue[startIndex..endIndex], out Int128 int128))
+    {
+        return int128;
+    }
+    else
+    {
+        throw new InvalidCastException("Invaid cast: " + encodedValue[startIndex..endIndex]);
+    }
+}
+
+static string DecodeString(string encodedValue, out int strLength, int startIndex = 0)
+{
+    var colonIndex = encodedValue.IndexOf(':');
+    if (colonIndex != -1)
+    {
+        //can pass back regular string length to account for the colon 
+        strLength = int.Parse(encodedValue[startIndex..colonIndex]);
+        return encodedValue.Substring(colonIndex + 1, strLength);
+    }
+    else
+    {
+        throw new InvalidOperationException("Invalid encoded value: " + encodedValue);
+    }
 }
