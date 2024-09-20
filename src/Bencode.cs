c997b65ab0f798006ff327600f3b1edbb80dfee6
stephenjones1337 <stevejonesesemail@gmail.com>
2024-09-20 17:05:36 -0500

    add Bencode class; decode dictionary; clean up some code

---
+++ src/Bencode.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_bittorrent.src
+{
+    public class Bencode
+    {
+        public static object Decode(string input)
+        {
+            return input[0] switch
+            {
+                var c when Char.IsDigit(c) => DecodeString(input),
+                'i' => DecodeInt(input),
+                'l' => DecodeList(input),
+                'd' => DecodeDictionary(input),
+                _ => throw new InvalidOperationException($"Could not decode {input}")
+            };
+        }
+
+        public static string Encode(object input)
+        {
+            return input switch
+            {
+                long n => $"i{n}e",
+
+                string s => $"{s.Length}:{s}",
+
+                object[] arr => $"l{string.Join("", arr.Select(Encode))}e",
+
+                object obj => EncodeNonPrimitiveType(input),
+
+                _ => throw new Exception($"Unknown type: {input.GetType().FullName}")
+            };
+        }
+
+        private static string EncodeNonPrimitiveType(object input) 
+        {
+            if (input is object[] inputArray)
+            {
+                return $"l{string.Join("", inputArray.Select(x => Encode(x)))}e";
+            }
+            else if (input is Dictionary<string, object> inputDictionary)
+            {
+                return $"d{string.Join("", inputDictionary.Values.Select(x => Encode(x)))}e";
+            }
+            else
+            {
+                throw new Exception($"Unknown type: {input.GetType().FullName}");
+            }
+        }
+
+        private static string DecodeString(string input)
+        {
+            int colonIndex = input.IndexOf(':');
+
+            if (colonIndex != -1)
+            {
+                var strlen = int.Parse(input[..colonIndex]);
+
+                return input.Substring(colonIndex + 1, strlen);
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid encoded value: " + input);
+            }
+        }
+
+        private static long DecodeInt(string input) => long.Parse(input[1..input.IndexOf('e')]); // EX i52e
+
+        private static object[] DecodeList(string input) // EX: l5:helloi52ee
+        {
+            //trim 'l'
+            input = input[1..];
+
+            var result = new List<object>();
+
+            //iterate while trimming input. ends when we hit 'e' which should be end...
+            while (input.Length > 0 && input[0] != 'e')
+            {
+                var element = Decode(input);
+
+                result.Add(element);
+
+                input = input[Encode(element).Length..];
+            }
+            
+            return [.. result];
+        }
+
+        private static Dictionary<string, object> DecodeDictionary(string input) // EX d<key1><value1>...<keyN><valueN>e
+        {
+            // key is always string, value could be anything...
+            //trim d
+            input = input[1..];
+
+            var result = new Dictionary<string, object>();
+
+            while(input.Length > 0 && input[0] != 'e')
+            {
+                var key = DecodeString(input);
+
+                input = input[Encode(key).Length..];
+
+                var value = Decode(input);
+
+                result.Add(key, value);
+
+                input = input[Encode(key).Length..];
+            }
+
+            return result;
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,4 @@
+using codecrafters_bittorrent.src;
 using System.Runtime.CompilerServices;
 using System.Text.Json;
 
@@ -12,99 +13,11 @@ var (command, param) = args.Length switch
 // Parse command and act accordingly
 if (command == "decode")
 {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    // Console.WriteLine("Logs from your program will appear here!");
-
-    // Uncomment this line to pass the first stage
     var encodedValue = param;
-    if (Char.IsDigit(encodedValue[0]))
-    {
-        // Example: "5:hello" -> "hello"
-        Console.WriteLine(DecodeString(encodedValue, out int strLength));
-    }
-    else if (Char.IsAsciiLetter(encodedValue[0]) && Char.IsAsciiLetter(encodedValue[^1])) // returns false for capital letters as well
-    {
-        //lists
-        if (encodedValue[0] == 'l' && encodedValue[^1] == 'e') //EX: l5:helloi52ee
-        {
-            // loop until reach e
-            //set current index
-            var currentIndex = 1;
-
-            while (currentIndex < encodedValue.Length-1)
-            {
-                // remaking first 'if' logic, come up with reusable way...
-                if (Char.IsDigit(encodedValue[currentIndex]))
-                {
-                    var str = DecodeString(encodedValue, out int strLength ,currentIndex);
-                    currentIndex += strLength;
-                    Console.WriteLine(str);
-                }
-                else if (encodedValue[currentIndex] == 'i') // i guess i'm hoping that only ints start with i...
-                {
-                    var intValue = DecodeInteger(encodedValue, out currentIndex, currentIndex);
-                    Console.WriteLine(intValue);
-                }
-                else
-                {
-                    //this should be end... we should break anyway to prevent recursion.
-                    //just throw error as we should never be here
-                    throw new InvalidDataException($"Data malformed, unable to continue processing loop. Current index: {currentIndex} - Current value: {encodedValue[currentIndex]} - Encoded value: " + encodedValue);
-                }
-            }
-
-            var colonIndex = encodedValue.IndexOf(':');
-        }
-        else
-        {
-            // Example: i52e
-            Console.WriteLine(JsonSerializer.Serialize(DecodeInteger(encodedValue, out int currentIndex)));
-        }
-    }
-    else
-    {
-        throw new InvalidOperationException("Unhandled encoded value: " + encodedValue);
-    }
+    
+    Console.WriteLine(JsonSerializer.Serialize(Bencode.Decode(encodedValue)));
 }
 else
 {
     throw new InvalidOperationException($"Invalid command: {command}");
-}
-
-
-
-static Int128 DecodeInteger(string encodedValue, out int currentIndex,  int startIndex = 1)
-{
-    int endIndex = startIndex;
-    foreach (char c in encodedValue)
-    {
-        if (Char.IsDigit(c))
-            endIndex++;
-        else if (Char.IsLetter(c))
-            break;
-    }
-    currentIndex = endIndex;
-    if (Int128.TryParse(encodedValue[startIndex..endIndex], out Int128 int128))
-    {
-        return int128;
-    }
-    else
-    {
-        throw new InvalidCastException("Invaid cast: " + encodedValue[startIndex..endIndex]);
-    }
-}
-
-static string DecodeString(string encodedValue, out int strLength, int startIndex = 0)
-{
-    var colonIndex = encodedValue.IndexOf(':');
-    if (colonIndex != -1)
-    {
-        //can pass back regular string length to account for the colon 
-        strLength = int.Parse(encodedValue[startIndex..colonIndex]);
-        return encodedValue.Substring(colonIndex + 1, strLength);
-    }
-    else
-    {
-        throw new InvalidOperationException("Invalid encoded value: " + encodedValue);
-    }
 }
